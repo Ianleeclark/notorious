ce141297740a3b92eda70c93af74a722b592af62
Ian Clark <ian@ianleeclark.com>
2016-03-17 22:45:11 -0500

    Issue #52: Hopefully docker deployment can fix this issue.

--- server/announce_response.go
+++ server/announce_response.go
@@ -45,13 +45,13 @@ func compactIPPort(b *bytes.Buffer, ip string, port string) (err error) {
 }
 
 func CompactAllPeers(ipport []string) []byte {
-	ret := bytes.NewBuffer(make([]byte, 0))
+	var ret bytes.Buffer
 	for i := range ipport {
 		sz := strings.Split(ipport[i], ":")
 		ip := sz[0]
 		port := sz[1]
 
-		err := compactIPPort(ret, ip, port)
+		err := compactIPPort(&ret, ip, port)
 		if err != nil {
 			panic(err)
 		}
@@ -73,6 +73,7 @@ func EncodeResponse(c *redis.Client, ipport []string, data *announceData) (resp 
 	ret += bencode.EncodeKV("incomplete", bencode.EncodeInt(incompleteCount))
 	if data.compact {
 		ipstr := string(CompactAllPeers(ipport))
+		fmt.Println(ipstr)
 		ret += bencode.EncodeKV("peers", ipstr)
 	} else {
 		return bencode.EncodePeerList(ipport)
