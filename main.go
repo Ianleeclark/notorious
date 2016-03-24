a1a6d3fc14fcc068581585740d8862cbcc024923
Ian Clark <ian@ianleeclark.com>
2016-03-23 21:26:21 -0500

    Issue #44: Decided to use viper for config parsing.

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"github.com/GrappigPanda/notorious/reaper"
 	"github.com/GrappigPanda/notorious/server"
+	"github.com/spf13/viper"
 	"time"
 )
+
+func loadConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("Failed to open config file")
+	}
+}
 
 func main() {
 	go reaper.StartReapingScheduler(5 * 60 * time.Second)
