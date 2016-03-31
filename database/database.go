e1d2e6d5f848d6de492b59cc703af090689a278d
Ian Clark <ian@ianleeclark.com>
2016-03-30 21:42:29 -0500

    Issue #25: Can't complete until further changes are made to Notorious.

--- database/database.go
+++ database/database.go
@@ -27,7 +27,7 @@ func OpenConnection() *gorm.DB {
 	return db
 }
 
-func ScrapeTorrent(db *gorm.DB, infoHash string) *gorm.DB {
+func ScrapeTorrent(db *gorm.DB, infoHash string) interface{} {
 	var torrent Torrent
-	return db.Where("infoHash = ?", infoHash).Find(&torrent)
+	return db.Where("infoHash = ?", infoHash).Find(&torrent).Value
 }
