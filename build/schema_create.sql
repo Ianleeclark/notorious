4b59f7e17ecad7643158bddaebda989ef4691cab
Ian Clark <ian@ianleeclark.com>
2016-04-27 22:30:09 -0500

    Issue #87: Changing the config.yaml for travis

--- build/schema_create.sql
+++ build/schema_create.sql
@@ -3,7 +3,7 @@ CREATE DATABASE testdb;
 
 USE testdb;
 
- GRANT USAGE ON *.* TO 'testuser'@'localhost';
+GRANT USAGE ON *.* TO 'testuser'@'localhost';
 DROP USER 'testuser'@'localhost';
 CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testuser';
 GRANT ALL PRIVILEGES ON testuser.* TO testuser@localhost;
