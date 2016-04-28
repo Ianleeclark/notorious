5e15133adcf40e8ceaa816f10418824c777aaa7c
Ian Clark <ian@ianleeclark.com>
2016-04-27 22:34:57 -0500

    Test

--- build/schema_create.sql
+++ build/schema_create.sql
@@ -3,8 +3,8 @@ CREATE DATABASE testdb;
 
 USE testdb;
 
-GRANT USAGE ON *.* TO 'testuser'@'localhost';
-DROP USER 'testuser'@'localhost';
-CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testuser';
-GRANT ALL PRIVILEGES ON testuser.* TO testuser@localhost;
+GRANT USAGE ON *.* TO 'testuser'@'%';
+DROP USER 'testuser'@'%';
+CREATE USER 'testuser'@'%' IDENTIFIED BY 'testuser';
+GRANT ALL PRIVILEGES ON testuser.* TO testuser@localhost WITH GRANT OPTION;
 FLUSH PRIVILEGES;
