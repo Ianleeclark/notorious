55352b10d4b0728862d7c75d7296e08cd5109d62
Ian Clark <ian@ianleeclark.com>
2016-12-11 14:00:20 -0600

    Issue #171
    CHANGE:
      - .travis.yml Updating build section to properly alter user name. Can't use
        `"` in `psql -c` for whatever reason.

--- database/impl/postgresStore.go
+++ database/impl/postgresStore.go
@@ -3,9 +3,10 @@ package sqlStoreImpl
 import (
 	"database/sql"
 	"github.com/GrappigPanda/notorious/database"
-	"github.com/GrappigPanda/notorious/database/mysql"
+	"github.com/GrappigPanda/notorious/database/postgres"
 	"github.com/GrappigPanda/notorious/database/schemas"
 	"github.com/jinzhu/gorm"
+	//"github.com/lib/pq"
 	// We use a blank import here because I'm afraid of breaking anything
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -20,7 +21,7 @@ type PostgresStore struct {
 // InitPostgresStore Creates a `PostgresStore` object and initiates all necessary
 // moving parts like the `HandlePeerUpdates` channel consumer.
 func InitPostgresStore() (store PostgresStore) {
-	dbConn, err := mysql.OpenConnection()
+	dbConn, err := postgres.OpenConnection()
 	if err != nil {
 		panic("Failed opening a connection to remote Postgres database.")
 	}
@@ -32,49 +33,65 @@ func InitPostgresStore() (store PostgresStore) {
 	return store
 }
 
-// OpenConnection wraps `mysql.OpenConnection`.
+// OpenConnection wraps `postgres.OpenConnection`.
 func (m *PostgresStore) OpenConnection() (*gorm.DB, error) {
-	return mysql.OpenConnection()
+	return postgres.OpenConnection()
 }
 
 // HandlePeerUpdates handles listening and aggregating peer updates. THis
 // allows block/asynchronous consumption of peer updates, rather than updating
 // the remote database at the end of every request.
 func (m *PostgresStore) HandlePeerUpdates() chan db.PeerTrackerDelta {
-	return nil
+	peerUpdatesChan := make(chan db.PeerTrackerDelta)
+
+	go func() {
+		for {
+			update := <-peerUpdatesChan
+			switch update.Event {
+			case db.PEERUPDATE:
+				m.UpdatePeerStats(update.Uploaded, update.Downloaded, update.IP)
+			case db.TRACKERUPDATE:
+				m.UpdateStats(update.Uploaded, update.Downloaded)
+			case db.TORRENTUPDATE:
+				m.UpdateTorrentStats(int64(update.Uploaded), int64(update.Downloaded))
+			}
+		}
+	}()
+
+	return peerUpdatesChan
 }
 
-// GetTorrent wraps `mysql.GetTorrent`.
+// GetTorrent wraps `postgres.GetTorrent`.
 func (m *PostgresStore) GetTorrent(infoHash string) (*schemas.Torrent, error) {
-	return mysql.GetTorrent(m.dbPool, infoHash)
+	return postgres.GetTorrent(m.dbPool, infoHash)
 }
 
-// GetWhitelistedTorrent wraps `mysql.GetWhitelistedTorrent`.
+// GetWhitelistedTorrent wraps `postgres.GetWhitelistedTorrent`.
 func (m *PostgresStore) GetWhitelistedTorrent(infoHash string) (*schemas.WhiteTorrent, error) {
-	return mysql.GetWhitelistedTorrent(m.dbPool, infoHash)
+	return postgres.GetWhitelistedTorrent(m.dbPool, infoHash)
 }
 
-// ScrapeTorrent wraps `mysql.ScrapeTorrent`.
+// ScrapeTorrent wraps `postgres.ScrapeTorrent`.
 func (m *PostgresStore) ScrapeTorrent(infoHash string) *schemas.Torrent {
-	return mysql.ScrapeTorrent(m.dbPool, infoHash)
+	return postgres.ScrapeTorrent(m.dbPool, infoHash)
 }
 
-// GetWhitelistedTorrents wraps `mysql.GetWhitelistedTorrents`.
+// GetWhitelistedTorrents wraps `postgres.GetWhitelistedTorrents`.
 func (m *PostgresStore) GetWhitelistedTorrents() (*sql.Rows, error) {
-	return mysql.GetWhitelistedTorrents(m.dbPool)
+	return postgres.GetWhitelistedTorrents(m.dbPool)
 }
 
-// UpdatePeerStats wraps `mysql.UpdatePeerStats`.
+// UpdatePeerStats wraps `postgres.UpdatePeerStats`.
 func (m *PostgresStore) UpdatePeerStats(uploaded uint64, downloaded uint64, ip string) {
-	mysql.UpdatePeerStats(m.dbPool, uploaded, downloaded, ip)
+	postgres.UpdatePeerStats(m.dbPool, uploaded, downloaded, ip)
 }
 
-// UpdateStats wraps `mysql.UpdateStats`.
+// UpdateStats wraps `postgres.UpdateStats`.
 func (m *PostgresStore) UpdateStats(uploaded uint64, downloaded uint64) {
-	mysql.UpdateStats(m.dbPool, uploaded, downloaded)
+	postgres.UpdateStats(m.dbPool, uploaded, downloaded)
 }
 
-// UpdateTorrentStats wraps `mysql.UpdateTorrentStats`.
+// UpdateTorrentStats wraps `postgres.UpdateTorrentStats`.
 func (m *PostgresStore) UpdateTorrentStats(uploaded int64, downloaded int64) {
-	mysql.UpdateTorrentStats(m.dbPool, uploaded, downloaded)
+	postgres.UpdateTorrentStats(m.dbPool, uploaded, downloaded)
 }
